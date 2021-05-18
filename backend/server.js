b6a78925b1324d881c97d688712a302cb9228f6e
timshoesmith <info@tjshoesmith.co.uk>
2021-05-18 12:35:08 +0100

    server to heroku

--- backend/server.js
+++ backend/server.js
@@ -26,15 +26,16 @@ app.get('/api/products/:id', (req, res) => {
     else
         res.status(404).send({msg: "Product Not Found."})
 });
-
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// });
 const port = process.env.PORT || 5000;
 app.listen({port}, () => {
     console.log(`server at http://localhost:${port}`);
